bbb35472a139cca3ab7527f4bebce182304168ce
Blavk_Mantis <121864774+Hack-Mantis@users.noreply.github.com>
2024-07-03 14:25:31 +0100

    Update firstcontract.sol
    /firstcontract. has been updated and bug errors of EVM version fixed. code was tested on metamask ethereum testnet.

--- firstcontract.sol
+++ firstcontract.sol
@@ -3,11 +3,36 @@ pragma solidity ^0.8.19; //solidity version
 
 contract firstcontract {
      
-     uint256 favoriteNumber;
+     uint256 public myFavoriteNumber; //default =0
 
-     function store(uint256 _favoriteNumber) public{
-        favoriteNumber = _favoriteNumber; 
+     struct person {
+        uint256 favoriteNumber;
+        string name;
+     }
+
+     person[] public listOfPeople;// []
+
+     mapping(string =>uint256) public nameToFavoriteNumber;
+
+    // person public jvchike = [{favoriteNumber: 77, name:"jvchike"}];
+    // person public neke = [{favoriteNumber: 25, name:"neke"}];
+    
+
+     function store (uint256 _favoriteNumber) public{
+        myFavoriteNumber = _favoriteNumber; 
     }
+     
+     function retriveve() public view returns(uint256){
+        return myFavoriteNumber;
+     }
+     
+     function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        listOfPeople.push( person({favoriteNumber: _favouriteNumber, name: _name}));
+        nameToFavoriteNumber[_name] = _favouriteNumber;
+     }
 
 
 }
+
+//this solidity code is a smart contract {firstcontract} that allows users to store and retrieve favorite numbers and add people with their favorite numbers and names. it includes functions to store a favorite number, retrieve the stored number, and add a person with a favorite number and name to the list.The contract also uses a mapping to associate names with favorite numbers. The code is designed to manage favorite numbers and people's information within the blockchain.
+//Github @hack-mantis 
